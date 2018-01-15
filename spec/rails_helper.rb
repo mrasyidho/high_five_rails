815121ca31ec6ff9097c4141bed847f8552221fe
Rasyid Ridho <mrasyidho01@gmail.com>
2018-01-15 14:56:42 +0700

    add todos_controller and items_controller, add spec both of them, and couple of helpers

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -22,7 +22,9 @@ require 'rspec/rails'
 # directory. Alternatively, in the individual `*_spec.rb` files, manually
 # require only the support files necessary.
 #
-# Dir[Rails.root.join('spec/support/**/*.rb')].each { |f| require f }
+
+Dir[Rails.root.join('spec/support/**/*.rb')].each { |f| require f }
+
 Shoulda::Matchers.configure do |config|
   config.integrate do |with|
     with.test_framework :rspec
@@ -79,4 +81,7 @@ RSpec.configure do |config|
       example.run
     end
   end
+
+  config.include RequestSpecHelper, type: :request
+
 end
